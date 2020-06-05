e4cb1ad77c8bc4e3a0bb95a51fcd126c9cf01e84
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-06-05 12:00:11 -0600

    still not there yet, but i feel like i am getting close. still stuck on the tabs file but the Header is done. need to commit push to submit sprint retro and i will need to resubmit later today after i finish this.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,38 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+function Tabs() {
+    const tabs = document.querySelector('.tabs')
+    
+    const topics = document.querySelector('.topics')
+
+    const tab = document.createElement('div')
+    tab.classList.add('tab')
+
+    tabs.appendChild(topics)
+    topics.appendChild(tab)
+
+
+    axios.get('https://lambda-times-backend.herokuapp.com/topics')
+        .then(response => {
+        // debugger
+        console.log('saul goodman', response.data.topics);
+
+        response.data.topics.forEach(item =>{
+            tabs.append(Tabs)
+        })
+        // create new tab forEach topic
+        // append to DOM under .topics
+        })
+        .catch(error => {
+        // debugger
+        console.log('something went wrong', error)
+        },[])
+
+        console.log('right after the whole promise thing')
+
+    return tabs;
+}
+
+Tabs();
