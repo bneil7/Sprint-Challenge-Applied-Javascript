4ab45947598d62af4cfc72a257aae43426c45759
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-06-05 17:09:28 -0600

    finished the Card file and project now meets MVP, ready to resubmit sprint retro

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,10 +19,48 @@
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
 
+function cardMaker(data){
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const headline = document.createElement('div')
+    headline.classList.add('headline')
+    headline.textContent = data.headline
+
+    const authorDiv = document.createElement('div')
+    authorDiv.classList.add('author')
+
+    const imgContainer = document.createElement('div')
+    imgContainer.classList.add('img-container')
+
+    const authorImg = document.createElement('img')
+    authorImg.src = data.authorPhoto
+
+    const authorName = document.createElement('span')
+    authorName.textContent = data.authorName
+    
+    card.appendChild(headline)
+    card.appendChild(authorDiv)
+    authorDiv.appendChild(imgContainer)
+    imgContainer.appendChild(authorImg)
+    authorDiv.appendChild(authorName)
+
+    return card;
+}
+
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
 .then(response => {
-  console.log('all gravy, davey', response.data.articles)
+    console.log('all gravy, davey', response.data.articles)
+    // returns object of arrays
+    // need to access value inside of objects keys
+    const arrData = Object.values(response.data.articles)
+    arrData.forEach(item => {
+        item.forEach(article => {
+            const entryPoint = document.querySelector('.cards-container')
+            entryPoint.appendChild(cardMaker(article))
+        })
+    })
 })
 .catch(error => {
   console.log('no bueno', error)
-},[])
+},[]);
